c1a3d574e419f9a6b39c9ac7e3da5ad04d73c070
alishbasarfraz17 <154826045+alishbasarfraz17@users.noreply.github.com>
2024-12-06 15:08:39 +0500

    Login-Signup
    Sign In & Sign Up Form

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js'
+import { getAuth } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js'
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js'
+
+
+
+
+
+export{
+    
+}
